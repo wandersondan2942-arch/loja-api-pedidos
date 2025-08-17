640fe6a0af4ede01d17ed16a00c1c5148a528f98
wandersondan2942-arch <wandersondan2942@gmail.com>
2025-08-17 12:33:04 -0300

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,7 @@
+echo "# loja-api-pedidos" >> README.md 
+git init 
+git add README.md 
+git commit -m "primeiro commit" 
+git branch -M main 
+git remote add origin https://github.com/wandersondan2942-arch/loja-api-pedidos.git
+ git push -u origin main
